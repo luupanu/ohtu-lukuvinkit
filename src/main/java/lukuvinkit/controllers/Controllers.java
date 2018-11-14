f69e07e7f10342acd5473d8903644fb5270e4781
Koppari <robin.stachon@helsinki.fi>
2018-11-14 14:20:37 +0200

    removed old mvc branch due to clutter, started a new one

--- src/main/java/lukuvinkit/controllers/Controllers.java
+++ src/main/java/lukuvinkit/controllers/Controllers.java
@@ -1,17 +1,31 @@
 package lukuvinkit.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import lukuvinkit.models.Lukuvinkki;
 
 // this can be turned into lukuvinkkicontroller if needed
 @Controller
 public class Controllers {
+    private ArrayList<Lukuvinkki> list;
+
+    // dummy before database setup
+    public Controllers() {
+        Lukuvinkki dummy = new Lukuvinkki("esim", "www.esim.com", "esim esim esim");
+        this.list = new ArrayList<Lukuvinkki>();
+        this.list.add(dummy);
+    }
 
     @GetMapping("/")
     @ResponseBody
-    public String home() {
-        return "<a href='/other'>linkki</a>";
+    public String home(Model model) {
+        model.addAttribute("list", list);
+        return "index";
     }
 
     @GetMapping("/other")
---
+++ src/main/java/lukuvinkit/models/Lukuvinkki.java
@@ -0,0 +1,39 @@
+package lukuvinkit.models;
+
+public class Lukuvinkki {
+
+    private String title;
+    private String url;
+    private String description;
+
+    public Lukuvinkki(String title, String url, String description) {
+        this.title = title;
+        this.url = url;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
