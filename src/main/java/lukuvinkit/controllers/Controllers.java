7de23464c62d8e5380976373afc8c02fa27fbbea
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2018-11-19 20:43:48 +0200

    Database functionality

--- src/main/java/lukuvinkit/controllers/Controllers.java
+++ src/main/java/lukuvinkit/controllers/Controllers.java
@@ -12,35 +12,62 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.validation.BindingResult;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import lukuvinkit.models.Lukuvinkki;
 
 // this can be turned into lukuvinkkicontroller if needed
 @Controller
 public class Controllers {
+    
+    public static final String DATABASE_ADDRESS = "jdbc:sqlite:data.db";
     private ArrayList<Lukuvinkki> list;
 
     // dummy before database setup
     @Autowired
-    public Controllers() {
-        Lukuvinkki dummy = new Lukuvinkki("esim", "https://www.esim.com", "esim esim esim");
-        this.list = new ArrayList<Lukuvinkki>();
-        this.list.add(dummy);
+    public Controllers() throws SQLException {
+        // Database initialization
+        Connection connection = DriverManager.getConnection(Controllers.DATABASE_ADDRESS);
+        
+        // Check if database table is already created.
+        PreparedStatement statementTableCheck = connection.prepareStatement(
+                "select count(*) from sqlite_master");
+        ResultSet result = statementTableCheck.executeQuery();
+        result.next();
+        int tableExists = result.getInt(1);
+        result.close();
+        statementTableCheck.close();
+        
+        // Create database table if it is not yet created.
+        if(tableExists == 0) {
+            PreparedStatement statementCreateTable = connection.prepareStatement(
+                    "CREATE TABLE Lukuvinkki(title TEXT, url TEXT, description TEXT)");
+            statementCreateTable.executeUpdate();
+            statementCreateTable.close();
+        }
+        
+        connection.close();
     }
 
     @GetMapping("*")
-    public String home(Model model) {
-        model.addAttribute("list", list);
+    public String home(Model model) throws SQLException {
+        ArrayList<Lukuvinkki> allBookmarks = Lukuvinkki.listAll();
+        model.addAttribute("list", allBookmarks);
         model.addAttribute("lukuvinkki", new Lukuvinkki());
         return "index";
     }
 
     @PostMapping(value = "/")
-    public String post(@Valid @ModelAttribute Lukuvinkki lukuvinkki, BindingResult bindingResult) {
+    public String post(@Valid @ModelAttribute Lukuvinkki lukuvinkki, BindingResult bindingResult) throws SQLException {
         if (bindingResult.hasErrors()) {
             return "index";
         }
 
-        this.list.add(lukuvinkki);
+        lukuvinkki.save();
         return "redirect:/";
     }
 }
--- src/main/java/lukuvinkit/models/Lukuvinkki.java
+++ src/main/java/lukuvinkit/models/Lukuvinkki.java
@@ -1,8 +1,18 @@
 package lukuvinkit.models;
 
+import lukuvinkit.controllers.Controllers;
+
 import org.springframework.stereotype.Component;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.URL;
+
+import java.util.ArrayList;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @Component
 public class Lukuvinkki {
@@ -49,6 +59,43 @@ public class Lukuvinkki {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+    
+    
+    // Database methods
+    
+    public void save() throws SQLException {
+        Connection connection = DriverManager.getConnection(Controllers.DATABASE_ADDRESS);
+        PreparedStatement statement = connection.prepareStatement(
+                "INSERT INTO Lukuvinkki(title, url, description) values (?, ?, ?)");
+        statement.setString(1, this.title);
+        statement.setString(2, this.url);
+        statement.setString(3, this.description);
+
+        int success = statement.executeUpdate();
+        System.out.println("SUCCESS: " + success);
+        
+        statement.close();
+        connection.close();
+    }
+    
+    public static ArrayList<Lukuvinkki> listAll() throws SQLException {
+        Connection connection = DriverManager.getConnection(Controllers.DATABASE_ADDRESS);
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM Lukuvinkki");
+        
+        ResultSet result = statement.executeQuery();
+        
+        ArrayList<Lukuvinkki> allBookmarks = new ArrayList<>();
+        
+        while (result.next()) {
+            allBookmarks.add(new Lukuvinkki(result.getString("title"), result.getString("url"), result.getString("description")));
+        } 
+            
+        result.close();
+        statement.close();
+        connection.close();
+        
+        return allBookmarks;
     }
 
 }
