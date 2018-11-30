e686c6c801b1d77dcfa3cb9df6acb3f496bc629b
Koppari <robin.stachon@helsinki.fi>
2018-11-30 11:08:11 +0200

    fixed so that if tips are hidden, they won't show up in search, showing tips requires a page reload

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -18,27 +18,43 @@ const toggleComments = (link) => {
 }
 
 function filterRead() {
-  let list, articles, i, read
+  let list,
+    articles,
+    i,
+    read,
+    hideCheck
 
+  hideCheck = document.getElementById("hideCheck")
   list = document.getElementById("list")
   articles = list.getElementsByTagName("article")
 
   /*Bug: 1. read tips are hidden 2. another tip is marked as read
   3. tips hidden by this function are shown again in the view
   this is because POST-call to index and page refresh?*/
+  // fix: make READ hide function call on page load?
 
   for (i = 0; i < articles.length; i++) {
     read = articles[i].getElementsByClassName("readingtips-read")
-    if (articles[i].style.display == "none") {
-      articles[i].style.display = ""
-    } else if (read.item(0).textContent.trim().indexOf("Mark as unread") > -1) {
+    if (read.item(0).textContent.trim().indexOf("Mark as unread") > -1) {
       articles[i].style.display = "none"
+      articles[i].dataset.marked = "true"
     }
+  }
+
+  if (hideCheck.textContent == "Hide read") {
+    hideCheck.textContent = "Show all"
+  } else {
+    location.reload()
   }
 }
 
 function searchInput() {
-  let input, filter, list, articles, i, tags
+  let input,
+    filter,
+    list,
+    articles,
+    i,
+    tags
 
   input = document.getElementById("search")
   filter = input
@@ -47,11 +63,14 @@ function searchInput() {
 
   list = document.getElementById("list")
   articles = list.getElementsByTagName("article")
-  
+
   for (i = 0; i < articles.length; i++) {
+    console.log(articles[i].dataset.marked);
     tags = articles[i].getElementsByClassName("readingtips-tags")
-    tags.item(0).textContent.trim().toUpperCase().indexOf(filter) > -1
-      ? articles[i].style.display = ""
-      : articles[i].style.display = "none"
+    if ((tags.item(0).textContent.trim().toUpperCase().indexOf(filter) > -1) && (articles[i].dataset.marked == "false")) {
+      articles[i].style.display = ""
+    } else {
+      articles[i].style.display = "none"
+    }
   }
 }
