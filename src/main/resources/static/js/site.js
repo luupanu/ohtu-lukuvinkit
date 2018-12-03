3ad0d96ed1dbaef4cc5f2f6c03b0f024665ce779
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2018-12-03 15:14:57 +0200

    simplified/refactored filtering of read tips

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,13 +1,15 @@
 let readingTipsList,
   individualTips
 
-const submitForm = (checkbox) => {
-  const form = checkbox.parentNode
+// Submits the parent form of given element.
+const submitForm = (element) => {
+  const form = element.parentNode
   form.submit()
-};
+}
 
-const toggleComments = (link) => {
-  const comments = link
+// Toggles the visibility of parent class '.commentarea'.
+const toggleComments = (element) => {
+  const comments = element
     .parentNode
     .querySelectorAll('.commentarea')
 
@@ -20,37 +22,21 @@ const toggleComments = (link) => {
   })
 }
 
-let filterRead = () => {
-  let i,
-    read,
-    hideCheck
+// Toggles the visibility of read tips.
+const filterRead = () => {
+  const hideRead = document.getElementById("filter-read")
+  const readTips = [...document.getElementsByClassName("tip-read")]
 
-  hideCheck = document.getElementById("hideCheck")
-  readingTipsList = document.getElementById("readingTipsList")
-  individualTips = readingTipsList.getElementsByTagName("article")
-
-  /*Bug: 1. read tips are hidden 2. another tip is marked as read
-  3. tips hidden by this function are shown again in the view
-  this is because POST-call to index and page refresh?*/
-  // fix: make READ hide function call on page load? Goes through all tips and
-  // checks if they've been read. If so, hide them.
-  for (i = 0; i < individualTips.length; i++) {
-    read = individualTips[i].getElementsByClassName("readingtips-read")
-    if (read.item(0).textContent.trim().indexOf("Mark as unread") > -1) {
-      individualTips[i].style.display = "none"
-      individualTips[i].dataset.marked = "true"
+  readTips.forEach(tip => {
+    if (hideRead.checked) {
+      tip.style.display = "none" 
+    } else {
+      tip.style.display = "block"
     }
-  }
-
-  // If tips have been hidden, make next click reload page.
-  if (hideCheck.textContent == "Hide read") {
-    hideCheck.textContent = "Show all"
-  } else {
-    location.reload()
-  }
+   })
 }
 
-let searchInput = () => {
+const searchInput = () => {
   let input,
     filter,
     i,
