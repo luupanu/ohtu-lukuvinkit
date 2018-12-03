15eb020c0ca6cf57e5f57a8938bb9471ca5c1159
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2018-12-03 18:33:57 +0200

    added a commented out option to only run specific features

--- src/test/java/ohtu/RunCukesTest.java
+++ src/test/java/ohtu/RunCukesTest.java
@@ -7,8 +7,11 @@ import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"},
-snippets = SnippetType.CAMELCASE)
+@CucumberOptions(
+  plugin = {"pretty"},
+  snippets = SnippetType.CAMELCASE
+  // , tags = "@only"
+)
 public class RunCukesTest {
     @ClassRule
     public static ServerRule server = new ServerRule(8080);
